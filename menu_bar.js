16cfd1f2bb90bddf78ac0edfe3b75e1b0d85a8bb
Jayanti Prasad <prasad.jayanti@gmail.com>
2022-01-19 16:49:23 +0530

    Initial commit

---
+++ menu_bar.js
@@ -0,0 +1,38 @@
+function  menu_bar(){
+ 
+ var menu='<ul id=\"mb2\"> <li><a href=\"https://jayantiprasad.github.io/\"> Home  </a> </li>'
+ +'<li><a href=\"research.html\"> Research</a>'
+ +'<ul> <li><a href="">Cosmology</a>'
+ +'<ul><li> <a href=\"nbody.html\">LSS </a></li>'
+ +'<li> <a href=\"cmbr.html\"> CMBR </a></li></ul>'
+ +'<li><a href=\"radio.html\">Radio Astronomy</a>'
+ +'<li><a href=\"gw.html\">Gravitational Waves</a>'
+ +'<li><a href=\"optimization.html\">Optimization</a>'
+ +'</ul>'
+ +'</li> <li><a href=\"\">Development</a>'
+ +'<ul> <li><a href=\"scipt.html\">Calculators</a>'
+ +'<ul> <li><a href=\"javascript:poptastic(\'jpscical.html\');\">Scientific Calculator </a></li>'
+ +'<li><a href=\"misc_cal.html\">Misc</a></li> </ul></li>'
+ +'<li><a href=\"scripting.html\">Scripting</a></li>'
+ +'<li><a href=\"parallel.html">HPC</a>'
+ +'<ul> <li><a href=\"mpi.html\">MPI</a></li>'
+ +'<li><a href=\"openmp.html\">OpenMP</a></li>'
+ +'<li><a href=\"pthread.html\">Pthreads</a></li>'
+ +'<li><a href=\"itbb.html\">ITBB</a></li>'
+ +'<li><a href=\"cuda.html\">CUDA</a></li>'
+ +'</ul></li> <li><a href=\"linux.html\">Linux</a></li>'
+ +'<li><a href=\"hpc.html\">HPC Training</a></li>'
+ +'<li><a href=\"craycx1.html\">CRAY-CX1</a></li>'
+ +'</ul></li> <li><a href=\"/js/mb/\">Personal</a>'
+ +'<ul> <li class=\"end\"><a href=\"fun_science.html">Fun Science </a></li>'
+ +'<li class=\"end\"><a href=\"pictures.html\">Pictures</a></li>'
+ +'</ul></li> <li><a href=\"resources.html\">Resources </a></li>'
+ +'<li><a href=\"download.html\">Download  </a></li>'
+ +'<li><a href=\"help.html\">Help </a></li>'
+ +'<li><a href=\"cv.html\">My CV </a> </li>'
+ +'<li><a href=\"flagcal.html\">FLAGCAL </a> </li></ul>'
++'<hr><br><br><br><br><br><br>';
+document.write(menu); 
+ 
+
+}
